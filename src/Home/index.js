2a17accca150a143245cd0b58c13add5310e6a11
Ankit Shah <shahankit2313@gmail.com>
2019-09-08 00:10:02 +0530

    Initial commit

---
+++ src/Home/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as s from './styles';
+
+function Home() {
+  return <div css={s.root}>Hello world</div>;
+}
+
+export default Home;
---
+++ src/Home/styles.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import { css } from '@emotion/core';
+
+export const root = css`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
